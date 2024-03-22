54588b94bfcaa39eb569d9fb57c48677bfb46426
mhshujon <monir@coderex.co>
2024-03-22 22:52:32 +0600

    initial library with EDD licensing

---
+++ src/EDD/EDDLicenseManager.php
@@ -0,0 +1,87 @@
+<?php
+namespace mhshujon\LicenseManager\EDD;
+
+use mhshujon\LicenseManager\LicenseManager;
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Manages license validation for a plugin using the Easy Digital Downloads (EDD) licensing system.
+ *
+ * @since 1.0.0
+ */
+class EDDLicenseManager extends LicenseManager
+{
+    /**
+     * Validates the license by making a request to the EDD licensing API.
+     *
+     * This method constructs API parameters and sends a POST request to the EDD licensing API endpoint.
+     * It sets the response data property with the result of the API call.
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function validate_license() {
+        $api_params = [
+            'edd_action' => "{$this->action}_license",
+            'license'    => $this->license,
+            'item_id'    => $this->item_id, // The ID of the item in RexTheme
+            'url'        => home_url()
+        ];
+
+        // Call the custom API.
+        $this->response_data = wp_remote_post(
+            $this->request_server,
+            [
+                'timeout' => 15,
+                'sslverify' => false,
+                'body' => $api_params
+            ]
+        );
+    }
+
+    /**
+     * Retrieves the response data after license validation.
+     *
+     * This method checks the response data received from the license validation request.
+     * It sets the success status and message based on the response.
+     *
+     * @since 1.0.0
+     *
+     * @return array The response data containing success status, license status, message, and response data.
+     */
+    public function get_response()
+    {
+        $success  = false;
+        $message = '';
+
+        $this->load_response_messages();
+
+        if (is_wp_error($this->response_data) || 200 !== wp_remote_retrieve_response_code($this->response_data)) {
+            $message = (is_wp_error($this->response_data) && !empty($this->response_data->get_error_message())) ? $this->response_data->get_error_message() : $this->error_messages[ 'default' ];
+        }
+        else {
+            $license_data = json_decode(wp_remote_retrieve_body($this->response_data));
+            if ( !empty( $license_data->success ) ) {
+                $success = true;
+            }
+            else {
+                $message = $this->error_messages[$license_data->error] ?? $this->error_messages['default'];
+            }
+        }
+
+        if ( empty( $message ) ) {
+            $message = $this->error_messages[ $this->action ] ?? $this->error_messages[ 'default' ];
+        }
+
+        return [
+            'success'        => $success,
+            'license_status' => $this->response_data->license ?? 'invalid',
+            'message'        => $message,
+            'response_data'  => $this->response_data
+        ];
+    }
+}
---
+++ src/LicenseManager.php
@@ -0,0 +1,157 @@
+<?php
+namespace mhshujon\LicenseManager;
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Manages license validation for a plugin using the Easy Digital Downloads (EDD) licensing system.
+ *
+ * @since 1.0.0
+ */
+abstract class LicenseManager
+{
+    /**
+     * @var string The action to perform (e.g., activate, deactivate, check).
+     *
+     * @since 1.0.0
+     */
+    protected $action;
+
+    /**
+     * @var int The ID of the item in RexTheme.
+     *
+     * @since 1.0.0
+     */
+    protected $item_id;
+
+    /**
+     * @var string The license key.
+     *
+     * @since 1.0.0
+     */
+    protected $license;
+
+    /**
+     * @var string The URL of the custom API server.
+     *
+     * @since 1.0.0
+     */
+    protected $request_server;
+
+    /**
+     * @var string The text domain for localization.
+     *
+     * @since 1.0.0
+     */
+    protected $text_domain;
+
+    /**
+     * @var string The name of the plugin.
+     *
+     * @since 1.0.0
+     */
+    protected $plugin_name;
+
+    /**
+     * @var array Error messages for different license validation outcomes.
+     *
+     * @since 1.0.0
+     */
+    protected $error_messages;
+
+    /**
+     * @var mixed The response data received after license validation.
+     *
+     * @since 1.0.0
+     */
+    protected $response_data;
+
+
+    /**
+     * Constructs a new LicenseManager instance.
+     *
+     * Initializes the LicenseManager object with provided parameters.
+     *
+     * @param string $action         The action to perform (e.g., activate, deactivate, check).
+     * @param int    $item_id        The ID of the item in RexTheme.
+     * @param string $license        The license key.
+     * @param string $request_server The URL of the custom API server.
+     * @param string $plugin_name    The name of the plugin.
+     * @param string $text_domain    The text domain for localization.
+     *
+     * @since 1.0.0
+     */
+    public function __construct( $action, $item_id, $license, $request_server, $plugin_name, $text_domain )
+    {
+        $this->action         = $action;
+        $this->item_id        = $item_id;
+        $this->license        = $license;
+        $this->request_server = esc_url( $request_server );
+        $this->plugin_name    = $plugin_name;
+        $this->text_domain    = $text_domain;
+    }
+
+    /**
+     * Loads response messages for different license validation actions.
+     *
+     * Sets error messages based on license validation outcomes, including activation,
+     * deactivation, validation, and various error scenarios.
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    protected function load_response_messages()
+    {
+        $this->error_messages = [
+            'activate'            => __('Your license has been successfully activated.', $this->text_domain),
+            'deactivate'          => __('Your license has been successfully deactivated.', $this->text_domain),
+            'check'               => __('Your license has been successfully validated.', $this->text_domain),
+            'revoked'             => __('Your license key has been disabled.', $this->text_domain),
+            'missing'             => __('Invalid license.', $this->text_domain),
+            'invalid'             => __('Your license is not active for this URL.', $this->text_domain),
+            'site_inactive'       => __('Your license is not active for this URL.', $this->text_domain),
+            'item_name_mismatch'  => sprintf(__('This appears to be an invalid license key for %s.', $this->text_domain), $this->plugin_name),
+            'no_activations_left' => __('Your license key has reached its activation limit.', $this->text_domain),
+            'deactivated'         => __('Your license successfully deactivate.', $this->text_domain),
+            'failed'              => __('Your license deactivation failed.', $this->text_domain),
+            'default'             => __('An unknown error occurred, please try again.', $this->text_domain)
+        ];
+
+        if ( !empty( $this->response_data->expires ) ) {
+            $this->error_messages[ 'expired' ] = sprintf(
+                __('Your license key expired on %s.', $this->text_domain),
+                date_i18n(
+                    get_option('date_format'),
+                    strtotime( $this->response_data->expires, current_time('timestamp'))
+                )
+            );
+        }
+    }
+
+    /**
+     * Validates the license by making a request to the EDD licensing API.
+     *
+     * This method constructs API parameters and sends a POST request to the EDD licensing API endpoint.
+     * It sets the response data property with the result of the API call.
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public abstract function validate_license();
+
+    /**
+     * Retrieves the response data after license validation.
+     *
+     * This method checks the response data received from the license validation request.
+     * It sets the success status and message based on the response.
+     *
+     * @since 1.0.0
+     *
+     * @return array The response data containing success status, license status, message, and response data.
+     */
+    public abstract function get_response();
+}
