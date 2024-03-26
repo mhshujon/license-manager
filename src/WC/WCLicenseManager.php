78d7f22aaff95b94cb1b07be17b4a66f343c9142
mhshujon <monir@coderex.co>
2024-03-26 12:51:53 +0600

    EDD licensing with proxy server

---
+++ src/WC/WCLicenseManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace mhshujon\LicenseManager\WC;
+
+use mhshujon\LicenseManager\LicenseManager;
+
+class WCLicenseManager extends LicenseManager
+{
+
+    public function validate_license()
+    {
+        // TODO: Implement validate_license() method.
+    }
+
+    public function get_response()
+    {
+        // TODO: Implement get_response() method.
+    }
+}
